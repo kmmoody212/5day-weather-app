fc6051cdc501303e3fb3b1a82d27fa10369089fd
kmmoody212 <kmfoster212@gmail.com>
2024-12-09 20:30:08 -0500

    Created logic for historyService

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,17 +1,107 @@
-// TODO: Define a City class with name and id properties
-
-// TODO: Complete the HistoryService class
+import fs from "node:fs/promises"; // file system from node to read files
+import { v4 as uuidv4 } from "uuid"; // uuid for unique id
+// City class with name and id properties
+class City {
+  name: string;
+  id: string;
+  weather:
+    | [
+        {
+          // weather object to retrieve from history without recalling api
+          name: string;
+          temperature: string;
+          date: string;
+          icon: string;
+          iconDescription: string;
+          tempF: string;
+          windSpeed: string;
+          humidity: string;
+        }
+      ]
+    | null;
+  constructor(name: string, id: string, weather = null) {
+    this.name = name;
+    this.id = id;
+    this.weather = weather;
+  }
+}
+// HistoryService class
 class HistoryService {
-  // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
-  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
-  // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
-  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  // Read method that reads from the searchHistory.json file
+  private async read() {
+    return await fs.readFile("db/searchHistory.json", {
+      flag: "r",
+      encoding: "utf8",
+    });
+  }
+  // Write method that writes the updated cities array to the searchHistory.json file
+  private async write(cities: City[]) {
+    return await fs.writeFile(
+      "db/searchHistory.json",
+      JSON.stringify(cities, null, "\t")
+    );
+  }
+  // getCities method that reads the cities from the searchHistory.json file uand returns them as an array of City objects
+  async getCities() {
+    return await this.read().then((citiesJson) => {
+      let parsedCities: City[];
+
+      try {
+        parsedCities = [].concat(JSON.parse(citiesJson));
+      } catch (err) {
+        parsedCities = [];
+      }
+
+      return parsedCities;
+    });
+  }
+  // addCity method that adds a city to the searchHistory.json file
+  async addCity(city: string, weather: any) {
+    if (!city) {
+      throw new Error("City can not be blank");
+    }
+    const citiesArray = await this.getCities(); // get cities array
+    let cityExists = false;
+    let existingCity = new City("", "", null);
+
+    for (let i = 0; i < citiesArray.length; i++) {
+      if (city.toLowerCase() === citiesArray[i].name.toLowerCase()) {
+        cityExists = true;
+        existingCity = citiesArray[i];
+        break;
+      }
+    }
+
+    if (!cityExists) {
+      const newCity: City = {
+        name: city,
+        id: uuidv4(),
+        weather,
+      };
+      console.log(
+        "this is a new city object with weather" + JSON.stringify(newCity)
+      );
+
+      return await this.getCities()
+        .then((parsedCities) => {
+          return [...parsedCities, newCity];
+        })
+        .then((updatedCities) => this.write(updatedCities))
+        .then(() => newCity);
+    } else {
+      console.log("City already exists: " + city);
+      return existingCity;
+    }
+  }
+
+  // removeCity method that removes a city from the searchHistory.json file
+  async removeCity(id: string) {
+    return await this.getCities()
+      .then((cities: City[]): City[] => {
+        return cities.filter((city) => city.id !== id);
+      })
+      .then((filteredCities) => this.write(filteredCities));
+  }
 }
 
 export default new HistoryService();
--- server/src/service/test.ts
+++ server/src/service/test.ts
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
-import { WeatherService } from "./weatherService";
+// import dotenv from "dotenv";
+// import { WeatherService } from "./weatherService";
 
-// import weatherService from "./weatherService";
-dotenv.config();
+// // import weatherService from "./weatherService";
+// dotenv.config();
 
-console.log("Start");
-console.log(process.env.API_KEY);
-console.log(process.env.API_BASE_URL);
-console.log("end");
+// console.log("Start");
+// console.log(process.env.API_KEY);
+// console.log(process.env.API_BASE_URL);
+// console.log("end");
 
-const service = new WeatherService(
-  "https://api.openweathermap.org",
-  "aa4091a0b7e4c9d37f87f34259e6c5e5"
-);
+// const service = new WeatherService(
+//   "https://api.openweathermap.org",
+//   "aa4091a0b7e4c9d37f87f34259e6c5e5"
+// );
 
-(async function () {
-  console.log(await service.getWeatherForCity("Raleigh"));
-})();
+// (async function () {
+//   console.log(await service.getWeatherForCity("Raleigh"));
+// })();
